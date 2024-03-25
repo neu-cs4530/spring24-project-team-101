ec2557736ba0b3273d05f512fb3fdd39bc7e0e7b
anjacd <71351017+anjacd@users.noreply.github.com>
2024-03-25 13:20:09 -0400

    skeleton of component and tests

---
+++ frontend/src/components/Town/interactables/DrawingArea.test.tsx
@@ -0,0 +1,10 @@
+import DrawingArea from './DrawingArea';
+
+describe('DrawingArea', () => {
+    it('should display the title', () => {
+
+    });
+    it('should display the drawing canvas', () => {
+
+    });
+});
---
+++ frontend/src/components/Town/interactables/DrawingArea.tsx
@@ -0,0 +1,11 @@
+import DrawingCanvas from './DrawingCanvas';
+
+export default function DrawingArea() {
+
+    return(
+        <>
+        <h3>Drawing Area</h3>
+        <DrawingCanvas />
+        </>
+    )
+}
