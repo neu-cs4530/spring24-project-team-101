3b3bfcd5b2970f9a9e105d169f03481a282bd98f
anjacd <71351017+anjacd@users.noreply.github.com>
2024-03-25 18:17:31 -0400

    prelim drawingarea and tests
    Only contains a DrawingCanvas in non-telestrations mode and a title. Tests check that these two elements are rendered. Will not run yet since drawingcanvas is not merged.

--- frontend/src/components/Town/interactables/DrawingArea.test.tsx
+++ frontend/src/components/Town/interactables/DrawingArea.test.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import DrawingArea from './DrawingArea';
+import { nanoid } from 'nanoid';
 
 describe('DrawingArea', () => {
-    it('should display the title', () => {
+  beforeEach(() => {
+    render(<DrawingArea interactableID={nanoid()} />);
+  });
 
-    });
-    it('should display the drawing canvas', () => {
+  it('should display the title', () => {
+    expect(screen.getAllByText('Drawing Area')).toHaveLength(1);
+  });
 
-    });
+  it('should display the drawing canvas', () => {
+    expect(screen.getAllByLabelText('drawing canvas')).toHaveLength(1);
+  });
 });
--- frontend/src/components/Town/interactables/DrawingArea.tsx
+++ frontend/src/components/Town/interactables/DrawingArea.tsx
@@ -1,11 +1,20 @@
 import DrawingCanvas from './DrawingCanvas';
+import DrawingAreaController from '../../../classes/interactable/DrawingAreaController';
+import { InteractableID } from '../../../types/CoveyTownSocket';
+import { useInteractableAreaController } from '../../../classes/TownController';
 
-export default function DrawingArea() {
+export default function DrawingArea({
+  interactableID,
+}: {
+  interactableID: InteractableID;
+}): JSX.Element {
+  const drawingAreaController =
+    useInteractableAreaController<DrawingAreaController>(interactableID);
 
-    return(
-        <>
-        <h3>Drawing Area</h3>
-        <DrawingCanvas />
-        </>
-    )
+  return (
+    <>
+      <h3>Drawing Area</h3>
+      <DrawingCanvas controller={drawingAreaController} />
+    </>
+  );
 }
