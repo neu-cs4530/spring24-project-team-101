bbc1483fd43051ba55b015bfd36d73b7f23887b7
jamessenior642 <jvsenior02@gmail.com>
2024-04-08 18:02:58 -0400

    frame almost working fully

--- frontend/src/components/Town/interactables/DrawingCanvas.tsx
+++ frontend/src/components/Town/interactables/DrawingCanvas.tsx
@@ -14,6 +14,17 @@ export type DrawingCanvasProps = {
   controller: GameAreaController<GameState, GameEventTypes>;
   authorID: string;
 };
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Image,
+} from '@chakra-ui/react';
 
 export default function DrawingCanvas({ controller, authorID }: DrawingCanvasProps): JSX.Element {
   const [color, setColor] = useState('#000000');
@@ -22,6 +33,24 @@ export default function DrawingCanvas({ controller, authorID }: DrawingCanvasPro
   const [saveData, setSaveData] = useState('');
   const [loading, setLoading] = useState(false);
   const toast = useToast();
+  const frameOptions = [
+    { id: 'none', label: 'No Frame', imageUrl: '' },
+    {
+      id: 'frame1',
+      label: 'Classic Frame',
+      imageUrl: '/images/pngtree-metallic-gold-picture-frame-3d-png-image_1046287.2.jpeg',
+    },
+    {
+      id: 'frame2',
+      label: 'Classic Frame',
+      imageUrl: '/images/pngtree-metallic-gold-picture-frame-3d-png-image_1046287.2.jpeg',
+    },
+    {
+      id: 'frame3',
+      label: 'Classic Frame',
+      imageUrl: '/images/pngtree-metallic-gold-picture-frame-3d-png-image_1046287.2.jpeg',
+    },
+  ];
 
   let telestrations: boolean;
   const controllerType = controller.toInteractableAreaModel().type;
@@ -41,6 +70,107 @@ export default function DrawingCanvas({ controller, authorID }: DrawingCanvasPro
   });
   const canvasRef = useRef(canvas);
   const handleColorChange = ({ hex }: ColorResult) => setColor(hex);
+
+  // FRAME FUNCTIONALITY
+  const [selectedFrame, setSelectedFrame] = useState(frameOptions[0]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalizeImage = async (imageUrl: string) => {
+    try {
+      await (controller as DrawingAreaController).makeMove({
+        drawingID: nanoid(),
+        authorID,
+        userDrawing: imageUrl, // Use the combined image URL
+      });
+      toast({
+        title: 'Drawing sent to gallery',
+        status: 'success',
+      });
+      onClose(); // Close the modal after successful processing
+    } catch (err) {
+      toast({
+        title: 'Error sending image to gallery',
+        description: (err as Error).toString(),
+        status: 'error',
+      });
+      // Do not close the modal here, allowing users to try again
+    } finally {
+      setLoading(false);
+    }
+  };
+  const sendDrawingToGallery = async () => {
+    if (!canvasRef.current) {
+      toast({
+        title: 'Error',
+        description: 'Canvas not initialized',
+        status: 'error',
+      });
+      return;
+    }
+    setLoading(true);
+    const offscreenCanvas = document.createElement('canvas');
+    const framePadding = 38; // This is the padding you want around your canvas image within the frame
+    offscreenCanvas.width = canvasRef.current.props.canvasWidth || 200;
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    +(2 * framePadding); // Add padding to the off-screen canvas dimensions
+    offscreenCanvas.height = canvasRef.current.props.canvasHeight || 200;
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    +(2 * framePadding);
+    const offscreenCtx = offscreenCanvas.getContext('2d');
+
+    if (selectedFrame.id !== 'none' && offscreenCtx) {
+      const frameImage = new window.Image();
+      frameImage.onload = () => {
+        // Draw the frame to fill the entire off-screen canvas
+        offscreenCtx.drawImage(frameImage, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+
+        const drawingImage = new window.Image();
+        drawingImage.onload = () => {
+          // Calculate the position to start drawing the canvas image
+          const xPosition = framePadding;
+          const yPosition = framePadding;
+
+          // Set the canvas image size smaller than the frame, subtracting the padding from both dimensions
+          const canvasImageWidth = offscreenCanvas.width - 2 * framePadding;
+          const canvasImageHeight = offscreenCanvas.height - 2 * framePadding;
+
+          // Draw the canvas image centered within the frame
+          offscreenCtx.drawImage(
+            drawingImage,
+            xPosition,
+            yPosition,
+            canvasImageWidth,
+            canvasImageHeight,
+          );
+          finalizeImage(offscreenCanvas.toDataURL('image/png'));
+        };
+        drawingImage.src = canvasRef.current.getDataURL('png', false, '#ffffff');
+      };
+      frameImage.src = selectedFrame.imageUrl;
+      frameImage.onerror = () => {
+        toast({
+          title: 'Error',
+          description: 'Failed to load frame image',
+          status: 'error',
+        });
+        setLoading(false);
+      };
+    } else if (offscreenCtx) {
+      // If 'none' is selected, just send the canvas drawing
+      const drawingImage = new window.Image();
+      drawingImage.onload = () => {
+        offscreenCtx.drawImage(drawingImage, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+        finalizeImage(offscreenCanvas.toDataURL('image/png'));
+      };
+      drawingImage.src = canvasRef.current.getDataURL('png', false, '#ffffff');
+    } else {
+      setLoading(false);
+      toast({
+        title: 'Error',
+        description: 'Context is not available',
+        status: 'error',
+      });
+    }
+  };
 
   return (
     <div>
@@ -153,29 +283,31 @@ export default function DrawingCanvas({ controller, authorID }: DrawingCanvasPro
       {telestrations ? (
         <></>
       ) : (
-        <Button
-          onClick={async () => {
-            setLoading(true);
-            const url = canvasRef.current.getDataURL('png', false, '#ffffff');
-            try {
-              await (controller as DrawingAreaController).makeMove({
-                drawingID: nanoid(),
-                authorID,
-                userDrawing: url,
-              });
-            } catch (err) {
-              toast({
-                title: 'Error sending image to gallery',
-                description: (err as Error).toString(),
-                status: 'error',
-              });
-            }
-            setLoading(false);
-          }}
-          isLoading={loading}
-          disabled={loading}>
-          Send to gallery
-        </Button>
+        <div>
+          <Button onClick={onOpen} disabled={loading}>
+            Send to Gallery
+          </Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Choose a Frame</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                {/* Dynamically generate frame selection buttons */}
+                {frameOptions.map(frame => (
+                  <Button key={frame.id} onClick={() => setSelectedFrame(frame)} m={2}>
+                    {frame.label}
+                  </Button>
+                ))}
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={sendDrawingToGallery}>
+                  Send with Frame
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </div>
       )}
     </div>
   );
